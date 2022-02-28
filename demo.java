e7956503578c9d5371fe4b5a0488c20bc8e585e9
NarayanaPondugula <99070110+NarayanaPondugula@users.noreply.github.com>
2022-02-28 13:42:51 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,9 +1,19 @@
+public class VowelConsonant {
 
-class Demo {
+    public static void main(String[] args) {
 
+        char ch = 'z';
 
-test coddeeeeeeeeeeeee
-test coeeee	
-hinaananfnkankfnkfnakfkn
-
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+        }
+    }
 }
